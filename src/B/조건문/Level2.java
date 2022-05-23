0c0dbb534c79c4cadc627cb7a2d2c86d64c36cb2
seongjaePark <92313032+seongjaePark12@users.noreply.github.com>
2022-05-23 13:47:51 +0900

    Update Level2.java

--- src/B/조건문/Level2.java
+++ src/B/조건문/Level2.java
@@ -16,11 +16,11 @@ public class Level2 {
 		Scanner sc = new Scanner(System.in);
 		int a;
 		a = sc.nextInt();
-    if(a>=90){System.out.println("A");}
-    else if(a>=80){System.out.println("B");}
-    else if(a>=70){System.out.println("C");}
-    else if(a>=60){System.out.println("D");}
-    else{System.out.println("F");}	
-    sc.close();
+    		if(a>=90){System.out.println("A");}
+    		else if(a>=80){System.out.println("B");}
+    		else if(a>=70){System.out.println("C");}
+    		else if(a>=60){System.out.println("D");}
+    		else{System.out.println("F");}	
+    		sc.close();
 	}
 }
