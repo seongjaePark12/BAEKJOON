a15fd2c88d0c650b6ed7093cdd2df4aa7514c362
seongjaePark <92313032+seongjaePark12@users.noreply.github.com>
2022-05-23 13:44:07 +0900

    Update Level12.java

--- src/A/입출력과사칙연산/Level12.java
+++ src/A/입출력과사칙연산/Level12.java
@@ -11,11 +11,11 @@ public class Level12 {
 		int a, b, c;
 		a = sc.nextInt();
 		b = sc.nextInt();
-        c = sc.nextInt();
+       		c = sc.nextInt();
 		System.out.println((a+b)%c);
-    System.out.println(((a%c) + (b%c))%c);
-    System.out.println((a*b)%c);
-    System.out.println(((a%c) * (b%c))%c);
-    sc.close();
+	   	System.out.println(((a%c) + (b%c))%c);
+	    	System.out.println((a*b)%c);
+	    	System.out.println(((a%c) * (b%c))%c);
+	    	sc.close();
 	}
 }
