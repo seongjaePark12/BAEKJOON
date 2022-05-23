9d2df22298dec441473774b7f9f72babde9c180e
seongjaePark <92313032+seongjaePark12@users.noreply.github.com>
2022-05-23 13:48:40 +0900

    Update Level4.java

--- src/B/조건문/Level4.java
+++ src/B/조건문/Level4.java
@@ -25,10 +25,10 @@ public class Level4 {
 		int a, b;
 		a = sc.nextInt();
 		b = sc.nextInt();
-    if(a>0 && b>0){System.out.println(1);}
-    else if(a<0 && b>0){System.out.println(2);}
-    else if(a>0 && b<0){System.out.println(4);}
-    else if(a<0 && b<0){System.out.println(3);}
-    sc.close();
+    		if(a>0 && b>0){System.out.println(1);}
+    		else if(a<0 && b>0){System.out.println(2);}
+    		else if(a>0 && b<0){System.out.println(4);}
+    		else if(a<0 && b<0){System.out.println(3);}
+    		sc.close();
 	}
 }
